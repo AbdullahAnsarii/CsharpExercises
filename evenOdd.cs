f2c9e2267d99fd1ff43c408b7d407dc43448eec4
Abdullah Ansari <aa9272137@gmail.com>
2020-08-14 03:23:23 +0500

    even odd lesson

---
+++ evenOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace evenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------Even/Odd Calculator----------------");
+            Console.WriteLine("Please Enter the numer:  ");
+            int input = int.Parse(Console.ReadLine());
+            int div = 0;
+            div = input % 2;
+            if (div == 0)
+            {
+                Console.WriteLine("The number is even!!!");
+
+            }
+            else
+            {
+                Console.WriteLine("The number is odd!!!");
+            }
+
+        }
+    }
+}
