d57f36bb22bceb14ed2dafa75fbe99fe25173c5f
Abdullah Ansari <aa9272137@gmail.com>
2020-08-14 03:37:17 +0500

    nested if

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nestedIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x == y)
+                Console.WriteLine("x=y");
+            else if (x != y)
+            {
+                if (x < y)
+                    Console.WriteLine("x<y");
+
+                else if (x > y)
+                    Console.WriteLine("x>y"); 
+
+            }
+            
+        }
+    }
+}
