9e24822a68e901f9fa011ee7d531b3265d86cc39
Abdullah Ansari <aa9272137@gmail.com>
2020-08-15 00:12:20 +0500

    enter any date and get the day on that date

---
+++ TimeMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TimeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------Time Machine-------------");
+            Console.WriteLine("Enter the day: ");
+            int xday = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the month: ");
+            int xmonth = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the year: ");
+            int xyear = int.Parse(Console.ReadLine());
+            DateTime fday = new DateTime(xyear, xmonth, xday);
+            Console.WriteLine("This day should be {0}", fday.DayOfWeek);
+
+
+
+
+        }
+    }
+}
