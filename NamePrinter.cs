1c3ade1ed92fd2cb29bca57a362910b7b8d8b3b2
Abdullah Ansari <aa9272137@gmail.com>
2020-08-15 00:03:46 +0500

    loop exercise & nameprinter string

---
+++ NamePrinter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string ans1 = Console.ReadLine();
+            Console.WriteLine("Nice to meet you {0} My name is C#, How old are you?", ans1 );
+            int ans2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} good, As for me I was born in 2002 which makes me {1} years old", ans2, DateTime.Now.Year-2002 );
+
+        }
+    }
+}
---
+++ loop.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace loopExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even Numbers Machine!!!");
+            for (int i = 1; i <= 100; i++)
+            {
+                //Console.WriteLine(i);
+                
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+                //i++;
+            }
+            //PAHAAR!!!
+            Console.WriteLine("\n -------PAHAAR-------");
+            string eggs = "0";
+            int counter = 1;
+            do
+            {
+                Console.WriteLine(eggs);
+                counter ++;
+                eggs = eggs + "0";
+            } while (counter <= 10);
+            Random rnd = new Random();
+            double k = 0;
+            k = rnd.NextDouble();
+            Console.WriteLine(k);
+
+            //Arrays
+
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] emptyArray = new int[5];
+            Console.WriteLine(array[2]);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("for se " + i);
+            }
+            foreach (var item in array)
+            {
+                Console.WriteLine("foreach se " + item);
+
+            }
+
+
+        
+        }
+    }
+}
